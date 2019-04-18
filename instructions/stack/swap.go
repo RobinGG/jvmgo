e5db89c84506dd13a7afe3001b37c140a4475eec
Robin Gong <i.robingong@gmail.com>
2019-04-18 23:08:38 +0800

    feature(instruction): rem, sh and swap instructions

---
+++ instructions/stack/swap.go
@@ -0,0 +1,21 @@
+package stack
+
+import (
+	"jvmgo/instructions/base"
+	"jvmgo/rtda"
+)
+
+type SWAP struct {
+	base.NoOperandsInstruction
+}
+
+func (self *SWAP) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	slot1 := stack.PopSlot()
+	slot2 := stack.PopSlot()
+	stack.PushSlot(slot1)
+	stack.PushSlot(slot2)
+}
+
+
+
